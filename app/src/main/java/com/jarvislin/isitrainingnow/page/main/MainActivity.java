0001fca59341cec50043fb2d6168ec64b6c58d4f
Jarvis Lin <jarvislin0202@gmail.com>
2017-01-27 22:18:17 +0800

    add info window

--- app/src/main/java/com/jarvislin/isitrainingnow/page/main/MainActivity.java
+++ app/src/main/java/com/jarvislin/isitrainingnow/page/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jarvislin.isitrainingnow.page.main;
 
-import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -8,11 +9,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.maps.android.clustering.ClusterManager;
 import com.jarvislin.isitrainingnow.BaseActivity;
 import com.jarvislin.isitrainingnow.R;
-import com.jarvislin.isitrainingnow.model.RainingData;
+import com.jarvislin.isitrainingnow.model.WeatherStation;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -23,11 +24,17 @@ import java.util.List;
 import timber.log.Timber;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends BaseActivity<MainPresenter> implements MainView, OnMapReadyCallback {
+public class MainActivity extends BaseActivity<MainPresenter> implements MainView, OnMapReadyCallback, ClusterManager.OnClusterItemInfoWindowClickListener<WeatherStation> {
 
     private GoogleMap map;
-    private static final LatLng TAIWAN = new LatLng(24.7724596,121.2679728);
-    private ClusterManager<RainingData> clusterManager;
+    private static final LatLng TAIWAN = new LatLng(24.122276688433903, 120.64848627895118);
+    private ClusterManager<WeatherStation> clusterManager;
+    private WeatherStation clickedStation;
+
+    @Override
+    protected MainPresenter createPresenter() {
+        return MainPresenter_.getInstance_(this);
+    }
 
     @AfterViews
     protected void init() {
@@ -37,48 +44,69 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainVie
     }
 
     @Override
-    protected MainPresenter createPresenter() {
-        return MainPresenter_.getInstance_(this);
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        CameraPosition position = new CameraPosition(TAIWAN, 9, 0, 0);
+        CameraPosition position = new CameraPosition(TAIWAN, 7, 0, 0);
         map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
 
         clusterManager = new ClusterManager<>(this, map);
+        clusterManager.getMarkerCollection().setOnInfoWindowAdapter(new StationInfoWindowAdapter());
+        clusterManager.setOnClusterItemInfoWindowClickListener(this);
+        clusterManager.setOnClusterItemClickListener(item -> {
+            clickedStation = item;
+            return false;
+        });
 
-        // Point the map's listeners at the listeners implemented by the cluster
-        // manager.
         map.setOnCameraIdleListener(clusterManager);
         map.setOnMarkerClickListener(clusterManager);
         map.setInfoWindowAdapter(clusterManager.getMarkerManager());
+        map.setOnInfoWindowClickListener(clusterManager);
 
-        presenter.fetchRainingData();
+        presenter.fetchWeatherStation();
     }
 
     @UiThread
     @Override
-    public void updateRainingData(List<RainingData> filteredList) {
-        Timber.e("size="+filteredList.size());
-        for(RainingData rainingData : filteredList) {
-//            map.addMarker(new MarkerOptions().position(rainingData.getLatLng()).title(rainingData.getName()));
-            clusterManager.addItem(rainingData);
+    public void updateStation(List<WeatherStation> filteredList) {
+        Timber.e("size=" + filteredList.size());
+        for (WeatherStation weatherStation : filteredList) {
+            clusterManager.addItem(weatherStation);
         }
         clusterManager.cluster();
 
         showToast(String.format("共監測到%s處降雨", filteredList.size()));
+    }
+
+    @Override
+    public void onClusterItemInfoWindowClick(WeatherStation weatherStation) {
+
+    }
+
+    class StationInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+        private final View view;
+
+        StationInfoWindowAdapter() {
+            view = getLayoutInflater().inflate(R.layout.info_window, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+
+            TextView name = ((TextView) view.findViewById(R.id.stationName));
+            TextView rainingInfo = ((TextView) view.findViewById(R.id.rainingInfo));
+            TextView publishTime = ((TextView) view.findViewById(R.id.publishTime));
+
+            name.setText(clickedStation.getName());
+            rainingInfo.setText(String.format(getString(R.string.raining_info), String.valueOf(clickedStation.getRainfall10min()), String.valueOf(clickedStation.getRainfall1hr())));
+            publishTime.setText(clickedStation.getPublishTime());
+
+            return view;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            return null;
+        }
     }
 }
